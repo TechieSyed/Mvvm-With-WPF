265e143ebfc87ba8278f20ff413840960b05c0e9
kihwanoh <kihwan2003.oh@hanmail.net>
2023-12-13 08:24:38 +0900

    WPF & MVVM 03 Creating Model Employee model을 만들고 System.ComponentModel의 INotifyPropertyChanged 인터페이스를 사용하여 변경사항이 반영되도록 한다.
    Helper function으로 private void OnPropertyChanged(string propertyName)을 만들어 각 항목에 대한 변경사항을 처리하도록 한다.
    
    Id, Name, Age등의 변수를 만든다.
    * propf를 입력하고 탭을 두번 누르면 property code block을 입력할 수 있다^^
    
    https://www.youtube.com/watch?v=D08Xf1xJJDk&list=PLEGjYQQO3ST8hatajWNDUGVwo437sPv8G&index=3

---
+++ MvvmDemo/Models/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+namespace MvvmDemo2.Models
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Employee() { }
+
+        //make helper funtion for INotifyPropertyChanged
+        private void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged("Id"); }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged("Name"); }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; OnPropertyChanged("Age"); }
+        }
+
+    }
+}
