65043fccd04a41a756816a5d13ff8296529bee92
kihwanoh <kihwan2003.oh@hanmail.net>
2023-12-13 14:34:12 +0900

    WPF & MVVM 07 Implementing Add operation using ViewModel Button입력을 처리하기위해 EmployeeView XAML엣서 각 Property에 현재 Employee정보를 바인딩해준다.
    이제 Button에 실행될때 처리할 명령을 새로운 클래스로 구현한다.
    - 액션을 담당할 델리게이트를 정의한다.
    - 액션은 실제 Command를 수행할 reference function을 전달해주어야 하므로 ViewModel에서 실제 command를 실행하는 함수를 구현하고 델리게이터에게 reference를 전달한다.
    - 명령 버튼에도 데이터 바인딩하여 해당 명령 실행자를 지정해준다.
    
    https://www.youtube.com/watch?v=GJlmEdp7Jyw&list=PLEGjYQQO3ST8hatajWNDUGVwo437sPv8G&index=7

---
+++ MvvmDemo/Commands/RelayCommands.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MvvmDemo.Commands
+{
+    public class RelayCommands : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action DoWork;
+        public RelayCommands(Action work)
+        {
+            DoWork = work;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            DoWork();
+        }
+    }
+}
--- MvvmDemo/ViewModels/EmployeeViewModel.cs
+++ MvvmDemo/ViewModels/EmployeeViewModel.cs
@@ -6,10 +6,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 using MvvmDemo.Models;
+using MvvmDemo.Commands;
 namespace MvvmDemo.ViewModels
 {
     public class EmployeeViewModel : INotifyPropertyChanged
     {
+        #region INotifyPropertyChanged_Implementation
         public event PropertyChangedEventHandler PropertyChanged;
 
         //여러 property를 위한 Helper function을 만든다.
@@ -17,14 +19,18 @@ namespace MvvmDemo.ViewModels
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        #endregion
 
         EmployeeService ObjEmployeeService;
         public EmployeeViewModel()
         {
             ObjEmployeeService = new EmployeeService();
             LoadData();
+            CurrentEmployee=new Employee();
+            saveCommand=new RelayCommands(Save);
         }
 
+        #region DisplayOperation
         private List<Employee> employeesList;
 
         public List<Employee> EmployeesList
@@ -37,6 +43,44 @@ namespace MvvmDemo.ViewModels
         {
             EmployeesList = ObjEmployeeService.GetAll();
         }
+        #endregion
 
+        private Employee currentEmployee;
+
+        public Employee CurrentEmployee
+        {
+            get { return currentEmployee; }
+            set { currentEmployee = value; OnChangedProperty("CurrentEmployee"); }
+        }
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; OnChangedProperty("Message"); }
+        }
+
+        private RelayCommands saveCommand;
+
+        public RelayCommands SaveCommand
+        {
+            get { return saveCommand; }
+        }
+        public void Save()
+        {
+            try
+            {
+                var IsSaved = ObjEmployeeService.Add(CurrentEmployee);
+                LoadData();
+                if (IsSaved)
+                    Message = "Employee saved";
+                else
+                    Message="Save operationfailed";
+            }
+            catch (Exception ex)
+            {
+                Message=ex.Message;
+            }
+        }
     }
 }
